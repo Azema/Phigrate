235f8ef3234d23654f09c9b863db9af695216806
Manuel HERVO <manuel.hervo@gmail.com>
2012-01-04 09:56:53 +0100

    Add environment and logger parameters in constructor. Add logs in methods. Add help for display usage of tasks. Remove error_handler (now it is in main.php) Add buffer to cache output of task execute(). Set directory of tasks in initTasks method. Modify parseArgs method to find args. DisplayResult is deplaced in main script.

--- lib/classes/class.Ruckusing_FrameworkRunner.php
+++ lib/classes/class.Ruckusing_FrameworkRunner.php
@@ -35,109 +35,121 @@ class Ruckusing_FrameworkRunner
      * 
      * @var array
      */
-	private $_db = null;
+    private $_db = null;
+
     /**
      * the currently active config 
      * 
      * @var mixed
      */
-	private $_activeDbConfig;
+    private $_activeDbConfig;
+
     /**
      * all available DB configs (e.g. test,development, production)
      * 
      * @var array
      */
-	private $_dbConfig = array();
+    private $_dbConfig = array();
+
     /**
      * task manager 
      * 
      * @var Ruckusing_TaskManager
      */
-	private $_taskMgr = null;
+    private $_taskMgr = null;
+
     /**
      * adapter 
      * 
      * @var Ruckusing_BaseAdapter
      */
-	private $_adapter = null;
+    private $_adapter = null;
+
     /**
      * current task name 
      * 
      * @var string
      */
-	private $_curTaskName = "";
+    private $_curTaskName;
+
+    /**
+     * Flag to display help of task
+     * @see Ruckusing_FrameworkRunner::_parseArgs
+     * 
+     * @var boolean
+     */
+    private $_helpTask = false;
+
     /**
      * task options 
      * 
      * @var string
      */
-	private $_taskOptions = "";
+    private $_taskOptions = '';
+
     /**
      * Environment
      * default is development 
-     * but can also be one 'test', 'production'
+     * but can also be one 'test', 'production', etc...
      * 
      * @var string
      */
-	private $_env = "development"; //
-	
+    private $_env = 'development';
+    
     /**
-	 * set up some defaults
+     * _logger 
      * 
-     * @var array
+     * @var Ruckusing_Logger
      */
-	private $_optMap = array('ENV' => 'development');
-	
+    private $_logger;
+    
     /**
      * __construct 
      * 
-     * @param array $db   Configs DB
-     * @param array $argv Arguments of the command line
+     * @param array            $db     Config environment of DBs
+     * @param array            $argv   Arguments of the command line
+     * @param string           $env    Environment
+     * @param Ruckusing_Logger $logger Instance logger
      *
      * @return Ruckusing_FrameworkRunner
      */
-    function __construct($db, $argv)
+    function __construct($db, $argv, $env, $logger)
     {
-		try {
-            set_error_handler(
-                array(
-                    'Ruckusing_FrameworkRunner', 
-                    'scrErrorHandler',
-                ), 
-                E_ALL
-            );
+        $this->setLogger($logger);
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $this->_env = $env;
+        try {
+            //include all adapters
+            $this->_loadAllAdapters(RUCKUSING_BASE . '/lib/classes/adapters');
+            // initialize DB
+            $this->_dbConfig = $db;
+            $this->initializeDb();
+            //parse arguments
+            $this->_parseArgs($argv);
+            $this->initTasks();
+        } catch (Exception $e) {
+            $this->_logger->err('Exception: ' . $e->getMessage());
+            throw $e;
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
 
-			//parse arguments
-			$this->_parseArgs($argv);
-
-			//initialize logger
-			$log_dir = RUCKUSING_BASE . "/logs";
-			if (is_dir($log_dir) && ! is_writable($log_dir)) {
-                die(
-                    "\n\nCannot write to log directory: "
-                    . "{$log_dir}\n\nCheck permissions.\n\n"
-                );
-			} else if (!is_dir($log_dir)) {
-				//try and create the log directory
-				mkdir($log_dir);
-			}
-			$log_name = sprintf('%s.log', $this->_env);
-            $this->logger = Ruckusing_Logger::instance(
-                $log_dir . '/' . $log_name
-            );
-			
-			//include all adapters
-			$this->_loadAllAdapters(RUCKUSING_BASE . '/lib/classes/adapters');
-			$this->_dbConfig = $db;
-			$this->initializeDb();
-			$this->initTasks();
-		} catch (Exception $e) {
-		}
-	}
-	
-	//-------------------------
-	// PUBLIC METHODS
-	//-------------------------	
+    /**
+     * setLogger 
+     * 
+     * @param Ruckusing_Logger $logger The logger
+     *
+     * @return Ruckusing_FrameworkRunner
+     */
+    public function setLogger(Ruckusing_Logger $logger)
+    {
+        $this->_logger = $logger;
+        return $this;
+    }
+    
+    //-------------------------
+    // PUBLIC METHODS
+    //------------------------- 
     /**
      * execute 
      * 
@@ -145,22 +157,28 @@ class Ruckusing_FrameworkRunner
      */
     public function execute()
     {
-		if ($this->_taskMgr->hasTask($this->_curTaskName)) {
-            $output = $this->_taskMgr->execute(
-                $this->_curTaskName, 
-                $this->_taskOptions
-            );
-			$this->_displayResults($output);
-			exit(0); // 0 is success
-		} else {
-			trigger_error(sprintf('Task not found: %s', $this->_curTaskName));
-			exit(1);
-		}
-		if ($this->logger) {
-            $this->logger->close();
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $output = '';
+        if ($this->_taskMgr->hasTask($this->_curTaskName)) {
+            if ($this->_helpTask) {
+                $output = $this->_taskMgr->help($this->_curTaskName);
+            } else {
+                ob_start();
+                $this->_taskMgr->execute(
+                    $this->_curTaskName, 
+                    $this->_taskOptions
+                );
+                $output = ob_get_clean();
+            }
+        } else {
+            $msg = 'Task not found: ' . $this->_curTaskName;
+            $this->_logger->err($msg);
+            throw new Ruckusing_InvalidTaskException($msg);
         }
-	}
-	
+        $this->_logger->debug(__METHOD__ . ' End');
+        return $output;
+    }
+    
     /**
      * initialize tasks 
      * 
@@ -168,9 +186,13 @@ class Ruckusing_FrameworkRunner
      */
     public function initTasks()
     {
-		$this->_taskMgr = new Ruckusing_TaskManager($this->_adapter);
-	}
-	
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $this->_taskMgr = new Ruckusing_TaskManager($this->_adapter);
+        // TODO : get directory of tasks from config file
+        $this->_taskMgr->setDirectoryOfTasks(RUCKUSING_BASE . '/lib/tasks');
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
+    
     /**
      * initialize db 
      * 
@@ -178,36 +200,33 @@ class Ruckusing_FrameworkRunner
      */
     public function initializeDb()
     {
-		try {
-			$this->_verifyDbConfig();			
-			$db = $this->_dbConfig[$this->_env];
-			$adapter = $this->_getAdapterClass($db['type']);
-			
-			if ($adapter === null) {
-                trigger_error(
-                    sprintf(
-                        'No adapter available for DB type: %s', 
-                        $db['type']
-                    )
-                );
-			}			
-			//construct our adapter			
-			$this->_adapter = new $adapter($db, $this->logger);
-		} catch (Exception $ex) {
-			trigger_error(sprintf("\n%s\n", $ex->getMessage()));
-		}
-	}
-	
+        $this->_logger->debug(__METHOD__ . ' Start');
+        try {
+            $this->_verifyDbConfig();
+            $configDb = $this->_dbConfig[$this->_env];
+            $this->_logger->debug('Config configDb ' . var_export($configDb, true));
+            $adapter = $this->_getAdapterClass($configDb['type']);
+            $this->_logger->debug('adapter ' . $adapter);
+            //construct our adapter         
+            $this->_adapter = new $adapter($configDb, $this->_logger);
+        } catch (Exception $ex) {
+            $this->_logger->err('Exception: ' . $ex->getMessage());
+            //trigger_error(sprintf("\n%s\n", $ex->getMessage()));
+            throw $ex;
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
+    
     /**
      * parse args 
-	 * $argv is our complete command line argument set.
-	 * PHP gives us: 
-	 * [0] = the actual file name we're executing
-	 * [1..N] = all other arguments
-	 * 
-	 * Our task name should be at slot [1] 
-	 * Anything else are additional parameters that we can pass
-	 * to our task and they can deal with them as they see fit.
+     * $argv is our complete command line argument set.
+     * PHP gives us: 
+     * [0] = the actual file name we're executing
+     * [1..N] = all other arguments
+     * 
+     * Our task name should be at slot [1] 
+     * Anything else are additional parameters that we can pass
+     * to our task and they can deal with them as they see fit.
      * 
      * @param array $argv Arguments of command line
      *
@@ -215,51 +234,40 @@ class Ruckusing_FrameworkRunner
      */
     private function _parseArgs($argv)
     {
-		$num_args = count($argv);
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $num_args = count($argv);
 
-		if ($num_args >= 2) {					
-			$this->_curTaskName = $argv[1];			
-			$options = array();
-			for ($i = 2; $i < $num_args;$i++) {
-				$arg = $argv[$i];
-				if (strpos($arg, '=') !== false) {
-					list($key, $value) = explode("=", $arg);
-					$options[$key] = $value;
-					if ($key == 'ENV') {
-						$this->_env = $value;
-					}
-				}
-			}
-			$this->_taskOptions = $options;
-		}
-	}
+        if ($num_args >= 2) {
+            $options = array();
+            for ($i = $num_args-1; $i >= 1; $i--) {
+                $arg = $argv[$i];
+                $this->_logger->debug(
+                    'arg: ' . $arg . ' - index: ' . $i
+                );
+                if (strpos($arg, ':') !== false) {
+                    $this->_curTaskName = $arg;
+                    continue;
+                } elseif ($arg == 'help') {
+                    $this->_helpTask = true;
+                    continue;
+                } elseif (strpos($arg, '=') !== false) {
+                    list($key, $value) = explode('=', $arg);
+                    $options[$key] = $value;
+                }
+            }
+            $options['ENV'] = $this->_env;
+            $this->_taskOptions = $options;
+        }
+        if ($num_args < 2 || !isset($this->_curTaskName)) {
+            throw new InvalidArgumentException('No task found!');
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
 
     /**
-     * error_handler 
-	 * Global error handler to process all errors during script execution
-     * 
-     * @param integer $errno   Error number
-     * @param string  $errstr  Error message
-     * @param string  $errfile Error file
-     * @param string  $errline Error line
-     *
-     * @return void
-     */
-    public static function scrErrorHandler($errno, $errstr, $errfile, $errline)
-    {
-        echo sprintf(
-            "\n\n(%s:%d) %s\n\n", 
-            basename($errfile), 
-            $errline, 
-            $errstr
-        );
-		exit(1); // exit with error
-	}
-	
-    /**
-	 * Update the local schema to handle multiple records versus the prior architecture
-	 * of storing a single version. In addition take all existing migration files
-	 * and register them in our new table, as they have already been executed.
+     * Update the local schema to handle multiple records versus the prior architecture
+     * of storing a single version. In addition take all existing migration files
+     * and register them in our new table, as they have already been executed.
      * 
      * @return void
      */
@@ -304,42 +312,9 @@ class Ruckusing_FrameworkRunner
         }
     }
 
-	//-------------------------
-	// PRIVATE METHODS
-	//-------------------------	
-    /**
-     * display results 
-     *
-     * @param string $output The output to display
-     * 
-     * @deprecated
-     *
-     * @return void
-     */
-    private function _displayResults($output)
-    {
-		return;
-		//deprecated
-		echo "\nStarted: " . date('Y-m-d g:ia T') . "\n\n";
-		echo "\n\n";
-		echo "\n$output\n";
-		echo "\nFinished: " . date('Y-m-d g:ia T') . "\n\n";
-	}
-	
-    /**
-     * set opt
-     * 
-     * @param string $key   The key of value
-     * @param mixed  $value The value to set
-     *
-     * @return void
-     */
-    private function _setOpt($key, $value)
-    {
-		if (!$key) return;
-		$this->_optMap[$key] = $value;		
-	}
-	
+    //-------------------------
+    // PRIVATE METHODS
+    //------------------------- 
     /**
      * verify db config 
      * 
@@ -348,44 +323,47 @@ class Ruckusing_FrameworkRunner
      */
     private function _verifyDbConfig()
     {
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $env = $this->_env;
         if (! is_array($this->_dbConfig) 
-            || ! array_key_exists($this->_env, $this->_dbConfig)
+            || ! array_key_exists($env, $this->_dbConfig)
         ) {
-            throw new Exception(
-                sprintf(
-                    "Error: '%s' DB is not configured",
-                    $this->_optMap[$ENV]
-                )
-            );
-		}
-		$env = $this->_env;
-		$this->_activeDbConfig = $this->_dbConfig[$env];
-		if (! array_key_exists('type', $this->_activeDbConfig)) {
-            throw new Exception(
-                sprintf("Error: 'type' is not set for '%s' DB", $env)
-            );
-		}
-		if (! array_key_exists('host', $this->_activeDbConfig)) {
-            throw new Exception(
-                sprintf("Error: 'host' is not set for '%s' DB", $env)
-            );			
-		}
-		if (! array_key_exists('database', $this->_activeDbConfig)) {
-            throw new Exception(
-                sprintf("Error: 'database' is not set for '%s' DB", $env)
-            );			
-		}
-		if (! array_key_exists('user', $this->_activeDbConfig)) {
-            throw new Exception(
-                sprintf("Error: 'user' is not set for '%s' DB", $env)
-            );			
-		}
-		if (! array_key_exists('password', $this->_activeDbConfig)) {
-            throw new Exception(
-                sprintf("Error: 'password' is not set for '%s' DB", $env)
-            );			
-		}
-	}
+            $msg = '(' . $env . ') DB is not configured!';
+            $this->_logger->err($msg);
+            throw new Ruckusing_MissingConfigDbException('Error: ' . $msg);
+        }
+        $this->_activeDbConfig = $this->_dbConfig[$env];
+        $exception = null;
+        if (! array_key_exists('type', $this->_activeDbConfig)) {
+            $msg = '"type" is not set for "' . $env . '" DB';
+            $this->_logger->err($msg);
+            $exception = new Ruckusing_MissingAdapterTypeException('Error: ' . $msg);
+        }
+        if (! array_key_exists('host', $this->_activeDbConfig)) {
+            $msg = '"host" is not set for "' . $env . '" DB';
+            $this->_logger->err($msg);
+            $exception = new Ruckusing_MissingConfigDbException('Error: ' . $msg);
+        }
+        if (! array_key_exists('database', $this->_activeDbConfig)) {
+            $msg = '"database" is not set for "' . $env . '" DB';
+            $this->_logger->err($msg);
+            $exception = new Ruckusing_MissingConfigDbException('Error: ' . $msg);
+        }
+        if (! array_key_exists('user', $this->_activeDbConfig)) {
+            $msg = '"user" is not set for "' . $env . '" DB';
+            $this->_logger->err($msg);
+            $exception = new Ruckusing_MissingConfigDbException('Error: ' . $msg);
+        }
+        if (! array_key_exists('password', $this->_activeDbConfig)) {
+            $msg = '"password" is not set for "' . $env . '" DB';
+            $this->_logger->err($msg);
+            $exception = new Ruckusing_MissingConfigDbException('Error: ' . $msg);
+        }
+        if (isset($exception)) {
+            throw $exception;
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
 
     /**
      * get adapter class 
@@ -396,28 +374,26 @@ class Ruckusing_FrameworkRunner
      */
     private function _getAdapterClass($dbType)
     {
-		$adapterClass = null;
-		switch (strtolower($dbType)) {
+        $adapterClass = null;
+        switch (strtolower($dbType)) {
         case 'mysql':
             $adapterClass = 'Ruckusing_MySQLAdapter';
             break;
         case 'mssql':
-            throw new Exception('Adapter not implemented!');
-            $adapterClass = 'Ruckusing_MSSQLAdapter';
-            break;
         case 'pgsql':
-            throw new Exception('Adapter not implemented!');
-            $adapterClass = 'Ruckusing_PostgresAdapter';
-            break;
-		}
-		return $adapterClass;
-	}
-	
+        default:
+            throw new Ruckusing_InvalidAdapterTypeException(
+                'Adapter "' . $dbType . '" not implemented!'
+            );
+        }
+        return $adapterClass;
+    }
+    
     /**
-	 * DB adapters are classes in lib/classes/adapters
-	 * and they follow the file name syntax of "class.<DB Name>Adapter.php".
-	 * 
-	 * See the function "_getAdapterClass" in this class for examples.
+     * DB adapters are classes in lib/classes/adapters
+     * and they follow the file name syntax of "class.<DB Name>Adapter.php".
+     * 
+     * See the function "_getAdapterClass" in this class for examples.
      * 
      * @param string $adapter_dir Directory path of adapters
      *
@@ -425,18 +401,18 @@ class Ruckusing_FrameworkRunner
      */
     private function _loadAllAdapters($adapter_dir)
     {
-		if (! is_dir($adapter_dir)) {
+        if (! is_dir($adapter_dir)) {
             trigger_error(
                 sprintf("Adapter dir: %s does not exist", $adapter_dir)
             );
-			return false;
-		}
-		$files = scandir($adapter_dir);
-		$regex = '/^class\.(\w+)Adapter\.php$/';
-		foreach ($files as $f) {			
-			//skip over invalid files
-			if ($f == '.' || $f == ".." || !preg_match($regex, $f)) continue;
-			include_once $adapter_dir . '/' . $f;
-		}
-	}
+            return false;
+        }
+        $files = scandir($adapter_dir);
+        $regex = '/^class\.(\w+)Adapter\.php$/';
+        foreach ($files as $f) {            
+            //skip over invalid files
+            if ($f == '.' || $f == ".." || !preg_match($regex, $f)) continue;
+            include_once $adapter_dir . '/' . $f;
+        }
+    }
 }
