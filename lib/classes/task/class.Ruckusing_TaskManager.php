618a195eb4ff778a75f02010c4a56b67e8b30a3f
Manuel HERVO <manuel.hervo@gmail.com>
2012-01-04 10:14:05 +0100

    Add logs in methods. Add method setDirectoryOfTasks. Replace ReflectionObject by instanceof in registerTask mathod. Add method help to display Usage of task. Modify execute method.

--- lib/classes/task/class.Ruckusing_TaskManager.php
+++ lib/classes/task/class.Ruckusing_TaskManager.php
@@ -18,9 +18,6 @@
  */
 require_once RUCKUSING_BASE . '/lib/classes/util/class.Ruckusing_NamingUtil.php';
 
-/** @var String Directory of tasks */
-define('RUCKUSING_TASK_DIR', RUCKUSING_BASE . '/lib/tasks');
-
 /**
  * Manager of tasks
  *
@@ -46,8 +43,22 @@ class Ruckusing_TaskManager
      * 
      * @var array
      */
-	private $_tasks = array();
-	
+    private $_tasks = array();
+
+    /**
+     * Directory of tasks 
+     * 
+     * @var string
+     */
+    private $_tasksDir;
+
+    /**
+     * _logger 
+     * 
+     * @var Ruckusing_Logger
+     */
+    private $_logger;
+    
     /**
      * __construct 
      * 
@@ -57,20 +68,42 @@ class Ruckusing_TaskManager
      */
     function __construct($adapter)
     {
-		$this->setAdapter($adapter);
-		$this->_loadAllTasks(RUCKUSING_TASK_DIR);
-	}
-	
+        $this->setAdapter($adapter);
+        $this->_logger = Ruckusing_Logger::instance();
+        $this->_loadAllTasks();
+    }
+
+    /**
+     * set directory of tasks 
+     * 
+     * @param string $tasksDir The path of directory of tasks
+     *
+     * @return Ruckusing_TaskManager
+     */
+    public function setDirectoryOfTasks($tasksDir)
+    {
+        $this->_logger->debug(__METHOD__ . ' Start');
+        if (! is_dir($taskDir)) {
+            $msg = 'Task dir: "' . $taskDir . '" does not exist!';
+            $this->_logger->err($msg);
+            throw new InvalidArgumentException($msg);
+        }
+        $this->_tasksDir = $tasksDir;
+        return $this;
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
+    
     /**
      * set adapter 
      *
      * @param Ruckusing_BaseAdapter $adapter Adapter RDBMS
      * 
-     * @return void
+     * @return Ruckusing_TaskManager
      */
     public function setAdapter($adapter) 
     {
-		$this->_adapter = $adapter;
+        $this->_adapter = $adapter;
+        return $this;
     }
 
     /**
@@ -80,25 +113,29 @@ class Ruckusing_TaskManager
      */
     public function getAdapter()
     {
-		return $this->_adapter;
-	}
-	
+        return $this->_adapter;
+    }
+    
     /**
-	 * Searches for the given task, and if found
-	 * returns it. Otherwise null is returned.
+     * Searches for the given task, and if found
+     * returns it. Otherwise null is returned.
      * 
      * @param mixed $key The key to identify the task
      *
-     * @return Ruckusing_iTask
+     * @return Ruckusing_ITask
+     * @throws Exception
      */
     public function getTask($key)
     {
-		if (array_key_exists($key, $this->_tasks)) {
-			return $this->_tasks[$key];
-		} else {
-			return null;
-		}
-	}
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $this->_logger->debug('TaskName: ' . $key);
+        if (! $this->hasTask($key)) {
+            $this->_logger->err('Task (' . $key . ') is not registered.');
+            throw new Exception("Task '{$key}' is not registered.");
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+        return $this->_tasks[$key];
+    }
 
     /**
      * has task 
@@ -109,45 +146,46 @@ class Ruckusing_TaskManager
      */
     public function hasTask($key)
     {
-		if (array_key_exists($key, $this->_tasks)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $exists = false;
+        if (array_key_exists($key, $this->_tasks)) {
+            $exists = true;
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+        return $exists;
+    }
 
     /**
-	 * Register a new task name under the specified key.
-	 * $obj is a class which implements the iTask interface
-	 * and has an execute() method defined.
+     * Register a new task name under the specified key.
+     * $obj is a class which implements the ITask interface
+     * and has an execute() method defined.
      * 
-     * @param string          $key The key to identify task
-     * @param Ruckusing_iTask $obj The task object
+     * @param string          $taskName The name of task
+     * @param Ruckusing_ITask $taskObj  The task object
      *
      * @return boolean
      */
-    public function registerTask($key, $obj)
+    public function registerTask($taskName, Ruckusing_ITask $taskObj)
     {
-		if (array_key_exists($key, $this->_tasks)) {
-			trigger_error(sprintf("Task key '%s' is already defined!", $key));
-			return false;
-		}
-		
-		//Reflect on the object and make sure it has an "execute()" method
-		$refl = new ReflectionObject($obj);
-		if (! $refl->hasMethod('execute')) {
-            trigger_error(
-                sprintf(
-                    "Task '%s' does not have an 'execute' method defined",
-                    $key
-                )
-            );
-			return false;
-		}
-		$this->_tasks[$key] = $obj;
-		return true;
-	}
-	
+        $this->_logger->debug(__METHOD__ . ' Start');
+
+        if ($this->hasTask($taskName)) {
+            $this->_logger->warn('Task name ' . $taskName . ' is already defined!');
+            trigger_error(sprintf("Task name '%s' is already defined!", $taskName));
+            return false;
+        }
+
+        if (! $taskObj instanceof Ruckusing_ITask) {
+            $msg = 'Task (' . $taskName . ') does not implement Ruckusing_ITask';
+            $this->_logger->warn($msg);
+            trigger_error($msg);
+            return false;
+        }
+        $this->_tasks[$taskName] = $taskObj;
+        $this->_logger->debug(__METHOD__ . ' End');
+        return true;
+    }
+    
     /**
      * get name 
      * 
@@ -155,11 +193,11 @@ class Ruckusing_TaskManager
      */
     public function getName()
     {
-	}
-	
-	//---------------------
-	// PRIVATE METHODS
-	//---------------------
+    }
+    
+    //---------------------
+    // PRIVATE METHODS
+    //---------------------
     /**
      * load all tasks 
      * 
@@ -168,39 +206,39 @@ class Ruckusing_TaskManager
      * @return void
      * @throws Exception
      */
-    private function _loadAllTasks($taskDir)
+    private function _loadAllTasks()
     {
-		if (!is_dir($taskDir)) {
-            throw new Exception(
-                sprintf("Task dir: %s does not exist", $taskDir)
-            );
-			return false;
-		}
-		$files = scandir($taskDir);
-		$regex = '/^class\.(\w+)\.php$/';
-		foreach ($files as $f) {
-			//skip over invalid files
-            if ($f == '.' || $f == ".." || !preg_match($regex, $f, $matches)) {
+        $this->_logger->debug(__METHOD__ . ' Start');
+        if (! isset($this->_tasksDir)) {
+            $msg = 'Please: you must specify the directory of tasks!';
+            $this->_logger->err($msg);
+            throw new InvalidArgumentException($msg);
+        }
+        if (! is_dir($this->_tasksDir)) {
+            $msg = 'Task dir: "' . $this->_tasksDir . '" does not exist!';
+            $this->_logger->err($msg);
+            throw new Exception($msg);
+        }
+        $files = scandir($this->_tasksDir);
+        $regex = '/^class\.(\w+)\.php$/';
+        foreach ($files as $f) {
+            //skip over invalid files
+            if ($f == '.' || $f == '..' || ! preg_match($regex, $f, $matches)) {
                 continue;
             }
-			include_once $taskDir . '/' . $f;
-			$taskName = Ruckusing_NamingUtil::taskFromClassName($matches[1]);
-			$klass = Ruckusing_NamingUtil::classFromFileName($f);
-			$this->registerTask($taskName, new $klass($this->getAdapter()));
-		}
-	}
-	
-	/**
-     * Execute the supplied Task object
-     *
-     * @param Ruckusing_iTask $taskObj Task object
-     *
-     * @return void
-	 */	
-    private function _executeTask($taskObj)
-    {
-	}
-	
+            $this->_logger->debug('include ' . $f);
+            include_once $this->_tasksDir . '/' . $f;
+            $taskName = Ruckusing_NamingUtil::taskFromClassName($matches[1]);
+            $this->_logger->debug('TaskName: ' . $taskName);
+            $klass = Ruckusing_NamingUtil::classFromFileName($f);
+            $this->_logger->debug('className ' . $klass);
+            $taskObj = new $klass($this->getAdapter());
+            $this->_logger->debug('obj: ' . get_class($taskObj));
+            $this->registerTask($taskName, $taskObj);
+        }
+        $this->_logger->debug(__METHOD__ . ' End');
+    }
+    
     /**
      * execute 
      * 
@@ -211,13 +249,26 @@ class Ruckusing_TaskManager
      */
     public function execute($taskName, $options)
     {
-		if (! $this->hasTask($taskName)) {
-			throw new Exception("Task '{$taskName}' is not registered.");
-		}
-		$task = $this->getTask($taskName);
-		if ($task) {
-			return $task->execute($options);
-		}
-		return '';
-	}
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $task = $this->getTask($taskName);
+        $output = $task->execute($options);
+        $this->_logger->debug(__METHOD__ . ' End');
+        return $output;
+    }
+    
+    /**
+     * Get display help of task
+     * 
+     * @param string $taskName The task name 
+     *
+     * @return string
+     */
+    public function help($taskName)
+    {
+        $this->_logger->debug(__METHOD__ . ' Start');
+        $task = $this->getTask($taskName);
+        $output = $task->help();
+        $this->_logger->debug(__METHOD__ . ' End');
+        return $output;
+    }
 }
