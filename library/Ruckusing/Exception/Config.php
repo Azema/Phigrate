f1b09724a8c7ec31f2019cbfaba84dc94c59baa1
Manuel HERVO <manuel.hervo@gmail.com>
2012-01-11 07:24:31 +0100

    Add class Config for loaded files INI

---
+++ library/Ruckusing/Exception/Config.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Rucksing Migrations
+ *
+ * PHP Version 5
+ *
+ * @category   RuckusingMigrations
+ * @package    Ruckusing_Exception
+ * @author     Cody Caughlan <codycaughlan % gmail . com>
+ * @author     Manuel HERVO <manuel.hervo % gmail .com>
+ * @copyright  2007 Cody Caughlan (codycaughlan % gmail . com)
+ * @license    GPLv2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @link       https://github.com/ruckus/ruckusing-migrations
+ */
+
+/**
+ * @see Ruckusing_Exception
+ */
+require_once 'Ruckusing/Exception.php';
+
+/**
+ * Class exception of invalid argument
+ *
+ * @category   RuckusingMigrations
+ * @package    Ruckusing_Exception
+ * @author     Cody Caughlan <codycaughlan % gmail . com>
+ * @author     Manuel HERVO <manuel.hervo % gmail .com>
+ * @copyright  2007 Cody Caughlan (codycaughlan % gmail . com)
+ * @license    GPLv2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @link       https://github.com/ruckus/ruckusing-migrations
+ */
+class Ruckusing_Exception_Config extends Ruckusing_Exception
+{
+}
+
